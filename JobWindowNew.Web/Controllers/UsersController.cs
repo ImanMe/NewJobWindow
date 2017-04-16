084f04f61c18eafe38320ec270e2a2e7db9fd617
Iman Mpr <iman.memarpour@gmail.com>
2017-04-16 01:44:33 -0400

    Wrap up

---
+++ JobWindowNew.Web/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobWindowNew.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public UsersController()
+        {
+        }
+
+        public UsersController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        // GET: Users
+        public ActionResult Index()
+        {
+            //var xx = _unitOfWork.UserRepository.GetUsers();
+            var result = UserManager.Users.ToList();
+            return View(result);
+        }
+    }
+}
