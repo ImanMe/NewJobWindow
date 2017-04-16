83c81e1dfd012490d6e2996be62f5cf12832fe29
Iman Mpr <iman.memarpour@gmail.com>
2017-04-16 02:50:34 -0400

    Api implemented I

---
+++ JobWindowNew.API/DTOs/ApplicantDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobWindowNew.API.DTOs
+{
+    public class ApplicantDTO
+    {
+        public int applicantID { get; set; }
+        public long listingID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public System.DateTime date { get; set; }
+        public Nullable<int> schedulingPod { get; set; }
+        public string File { get; set; }
+        public string listingTitle { get; set; }
+        public Nullable<int> officeID { get; set; }
+        public Nullable<int> specificSourceID { get; set; }
+        public string specificSourceName { get; set; }
+
+
+        public string Resume
+        {
+            get
+            {
+                return "http://board.thejobwindow.com/Resumes/" + File;
+            }
+        }
+    }
+}
---
+++ JobWindowNew.API/DTOs/ApplicantFactory.cs
@@ -0,0 +1,31 @@
+﻿using JobWindowNew.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobWindowNew.API.DTOs
+{
+    public class ApplicantFactory
+    {
+        public ApplicantDTO Create(ApplicantApstream applicant)
+        {
+            return new ApplicantDTO()
+            {
+                applicantID = (int)applicant.Id,
+                listingID = applicant.listingID,
+                firstName = applicant.FirstName,
+                lastName = applicant.LastName,
+                email = applicant.Email,
+                phone = applicant.Phone,
+                date = applicant.date,
+                schedulingPod = applicant.SchedulingPod,
+                File = "http://board.thejobwindow.com/Resumes/" + applicant.File,
+                listingTitle = applicant.listingTitle,
+                officeID = applicant.OfficeId,
+                specificSourceID = applicant.specificSourceID,
+                specificSourceName = applicant.specificSourceName,
+            };
+        }
+    }
+}
