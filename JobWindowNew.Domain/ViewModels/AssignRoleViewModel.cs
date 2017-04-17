70b8f68d89a24946f7b96459fe91dd09035dcce7
Iman Mpr <iman.memarpour@gmail.com>
2017-04-16 23:46:46 -0400

    Assign role name fix

--- JobWindowNew.Domain/ViewModels/AssignRoleViewModel.cs
+++ JobWindowNew.Domain/ViewModels/AssignRoleViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using JobWindowNew.Domain.Model;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace JobWindowNew.Domain.ViewModels
@@ -8,11 +9,13 @@ namespace JobWindowNew.Domain.ViewModels
     public class AssignRoleViewModel
     {
         [Required]
+        [DisplayName("Role")]
         public string RoleName { get; set; }
 
         public IEnumerable<IdentityRole> Roles { get; set; }
 
         [Required]
+        [DisplayName("User Name")]
         public string UserId { get; set; }
 
         public IEnumerable<ApplicationUser> Users { get; set; }
